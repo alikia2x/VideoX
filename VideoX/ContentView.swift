d94fb97306b170b9846304454ca386859de43131
Alikia2x <ziwen243@outlook.com>
2024-06-10 01:27:19 +0800

    Initial Commit

---
+++ VideoX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoX
+//
+//  Created by Alikia2x on 2024/6/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoX/VideoXApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoXApp.swift
+//  VideoX
+//
+//  Created by Alikia2x on 2024/6/10.
+//
+
+import SwiftUI
+
+@main
+struct VideoXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
