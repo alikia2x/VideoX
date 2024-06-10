5c950ac89d5f16207d115a2c83f8e0c1e768c4db
Alikia2x <ziwen243@outlook.com>
2024-06-10 21:36:28 +0800

    fix: now app cleans temp file while launching

--- VideoX/VideoXApp.swift
+++ VideoX/VideoXApp.swift
@@ -7,8 +7,25 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Do clean when load
+        let outputURL = URL.documentsDirectory.appending(path: "compressed.mp4")
+        if FileManager.default.fileExists(atPath: outputURL.path) {
+            do {
+                try FileManager.default.removeItem(at: outputURL)
+                print("Launch clean successfully operated.")
+            } catch {
+                print("Error removing existing file: \(error.localizedDescription)")
+            }
+        }
+        return true
+    }
+}
+
 @main
 struct VideoXApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             MainView()
